87059c8fb97a0903ef1e9f4e25b15d9e5b8a7f9f
Kristin Whetstone <kwhetstone@cloudbees.com>
2016-10-24 23:50:31 -0400

    Remove xvnc

--- helper.groovy
+++ helper.groovy
@@ -2,10 +2,10 @@ def test(dockerImg) {
     def testImg = docker.build('kwhetstone/ato_demo_test:snapshot', 'test')
     dockerImg.withRun { img ->
         testImg.inside("--link=${img.id}:testing") {
-            wrap([$class: 'Xvnc', takeScreenshot: true, useXauthority: true]) {
+            //wrap([$class: 'Xvnc', takeScreenshot: true, useXauthority: true]) {
                 sh "npm test"
                 junit '*.xml'
-            }
+            //}
         }
     }
 }
