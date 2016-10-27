4511c519e914007ebb8e377f2fdf65e5ad06c08e
Kristin Whetstone <kwhetstone@cloudbees.com>
2016-10-27 13:20:06 -0400

    Renable xvnc

--- helper.groovy
+++ helper.groovy
@@ -2,10 +2,11 @@ def test(dockerImg) {
     def testImg = docker.build('kwhetstone/ato_demo_test:snapshot', 'test')
     dockerImg.withRun { img ->
         testImg.inside("--link=${img.id}:testing") {
-            //wrap([$class: 'Xvnc', takeScreenshot: true, useXauthority: true]) {
+            //to use Xvnc, download the associated plugin, and start at least 1 time on your desired executor
+            wrap([$class: 'Xvnc', takeScreenshot: true, useXauthority: true]) {
                 sh "npm test"
                 junit '*.xml'
-            //}
+            }
         }
     }
 }
